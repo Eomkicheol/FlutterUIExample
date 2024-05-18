ce9eeac6b409562b925a435bb325912a3cc5ab08
Dominic(Dongkyu)Lee <ciuizz@naver.com>
2024-05-19 00:18:30 +0900

    card flip with single ticker animation / transform with matrix4

--- card_flip/lib/main.dart
+++ card_flip/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(const MyApp());
@@ -29,13 +30,48 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+      reverseDuration: Duration(seconds: 1),
+    );
+    _controller.addListener(() {
+      _colorChange();
     });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Color boxColor = Colors.red;
+
+  void _colorChange(){
+    if(_controller.value > 0.5){
+      setState(() {
+        boxColor = Colors.black;
+      });
+    } else {
+      setState(() {
+        boxColor = Colors.red;
+      });
+    }
+  }
+
+  void _flipCard(){
+    if (_controller.isCompleted) {
+      _controller.reverse();
+    } else {
+      _controller.forward();
+    }
   }
 
   @override
@@ -48,12 +84,35 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                double angle = _controller.value * math.pi;
+                Matrix4 transform = Matrix4.identity()
+                  ..setEntry(3, 2, 0.003) // Perspective
+                  ..rotateY(angle);
+                return Transform(
+                  transform: transform,
+                  alignment: Alignment.center,
+                  child: child,
+                );
+              },
+              child: Container(
+                  width : MediaQuery.of(context).size.width / 2,
+                  height : MediaQuery.of(context).size.height / 3,
+                  decoration : BoxDecoration(
+                      color : boxColor,
+                      borderRadius: BorderRadius.circular(15)
+                  ),
+              )
             ),
-            Text(
-              "Reveal"
-            ),
+            SizedBox(height: 30,),
+            GestureDetector(
+              onTap: (){
+                _flipCard();
+              },
+              child: Text("Reveal", style: TextStyle(color: Colors.blueAccent, fontSize: 18),),
+            )
           ],
         ),
       ),
